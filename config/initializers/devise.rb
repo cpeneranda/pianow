551c3f89c0e2ac5282f345d635b6570bb2488051
clement peneranda <clement@peneranda.fr>
2017-05-23 12:37:21 +0200

    sign in ok

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'clement@peneranda.fr'
+  config.mailer_sender = 'peneranda@icloud.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
