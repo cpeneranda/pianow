119374fa882e298c053fbb8394239d5cce5c07e8
clement peneranda <clement@peneranda.fr>
2017-05-23 10:19:45 +0200

    Facebook login

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,5 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
-  controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
