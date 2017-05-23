1a6fe36f20e26de513b6a6bf22086da2cafc8679
clement peneranda <clement@peneranda.fr>
2017-05-23 09:36:57 +0200

    Facebook login

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -251,6 +251,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+Devise.setup do |config|
   config.omniauth :facebook, ENV["FB_ID"], ENV["FB_SECRET"],
     scope: 'email',
     info_fields: 'email, first_name, last_name',
